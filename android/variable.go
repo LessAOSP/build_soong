62f1b1398d8b7e7477aa3da08538cc5245a5b402
aghora7 <ukikieran@gmail.com>
2025-06-14 14:05:30 +0530

    soong: add our changes

--- android/variable.go
+++ android/variable.go
@@ -1003,8 +1003,8 @@ func createVariableProperties(moduleTypeProps []interface{}, productVariables in
 func createVariablePropertiesType(moduleTypeProps []interface{}, productVariables interface{}) reflect.Type {
 	typ, _ := proptools.FilterPropertyStruct(reflect.TypeOf(productVariables),
 		func(field reflect.StructField, prefix string) (bool, reflect.StructField) {
-			if strings.HasPrefix(prefix, "Product_variables.Voltage") {
-				// Convert Product_variables.Voltage.Foo to Voltage.Foo
+			if strings.HasPrefix(prefix, "Product_variables.LessAOSP") {
+				// Convert Product_variables.LessAOSP.Foo to LessAOSP.Foo
 				_, prefix = splitPrefix(prefix)
 			}
 
